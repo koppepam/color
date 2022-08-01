c49764cbe7deed796aa61a5a26f256e6caef1cc8
yoneyamarise <102212479+yoneyamarise@users.noreply.github.com>
2022-08-01 19:41:25 +0900

    まとめた

--- color.js
+++ color.js
@@ -3,61 +3,31 @@ totalSummer = document.getElementById('total-summer');
 totalAutumn = document.getElementById('total-autumn');
 totalWinter = document.getElementById('total-winter');
 
-const assessmentButton = document.getElementById
-('assessment');
+const assessmentButton = document.getElementById('assessment');
 
-//春
+function counter(elements) {
+  let count = 0
+  for (let i = 0; i < elements.length; i++){
+    if(elements[i].checked){
+      count++
+    }
+  }
+  return count;
+}//カウント
+
+
 const spring = document.getElementsByClassName('spring');
-const getCheckedCountSp = () =>{
-  let sp = 0
-  for (let a = 0; a < spring.length; a++){
-    if(spring[a].checked){
-      sp++
-    }
-  }
-  totalSpring.innerText = (sp);
-};
-
-//夏
 const summer = document.getElementsByClassName('summer');
-const getCheckedCountSm = () =>{
-  let sm = 0
-  for(let b = 0; b < summer.length; b++){
-    if(summer[b].checked){
-      sm++
-    }
-  }
-  totalSummer.innerText = (sm);
-};
-
-//秋
 const autumn = document.getElementsByClassName('autumn');
-const getCheckedCountAu = () =>{
-  let au = 0
-  for(let c = 0; c < autumn.length; c++){
-    if(autumn[c].checked){
-      au++
-    }
-  }
-  totalAutumn.innerText = (au);
-};
-
-//冬
 const winter = document.getElementsByClassName('winter');
-const getCheckedCountWi = () =>{
-  let wi = 0
-  for(let d = 0; d < winter.length; d++){
-    if(winter[d].checked){
-      wi++
-    }
-  }
-  totalWinter.innerText = (wi);
-};
 
-assessmentButton.addEventListener("click",getCheckedCountSp,false); //春
-assessmentButton.addEventListener("click",getCheckedCountSm,false); //夏
-assessmentButton.addEventListener("click",getCheckedCountAu,false); //秋
-assessmentButton.addEventListener("click",getCheckedCountWi,false); //冬
+assessmentButton.addEventListener("click", () => {
+  totalSpring.innerText = counter(spring);//春
+  totalSummer.innerText = counter(summer);//夏
+  totalAutumn.innerText = counter(autumn);//秋
+  totalWinter.innerText = counter(winter);//冬
+}); 
+
 
 //特徴を見るボタン
 document.getElementById('detail-spring').addEventListener("click",function(){
